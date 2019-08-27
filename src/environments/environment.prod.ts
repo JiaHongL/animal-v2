03e51c278905d7864e35ff7dfecdb88117b07fc8
Joe Lin <>
2019-08-27 16:13:57 +0800

    [其他] 調整 .gitignore

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA34Fs1ewuJseFLcguzFu4Zx2FWGxdc98A',
+    authDomain: 'animal-backend.firebaseapp.com',
+    databaseURL: 'https://animal-backend.firebaseio.com',
+    projectId: 'animal-backend',
+    storageBucket: 'animal-backend.appspot.com',
+    messagingSenderId: '441367574674'
+  },
+  api: {
+    path: 'https://animal-proxy-api.herokuapp.com'
+  }
+};
