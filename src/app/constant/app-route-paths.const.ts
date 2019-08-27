2e788fc8fa4fd393d6ab75a6bfe4416ad30c9788
Joe Lin <>
2019-08-27 13:30:11 +0800

    [建置] 新增 全系統路由 Path & Title 定義檔

---
+++ src/app/constant/app-route-paths.const.ts
@@ -0,0 +1,79 @@
+/**
+ * 全系統路由 Path & Title 定義檔
+ *
+ */
+export const appRoutePaths = {
+
+  /**
+   * 管理員登入
+   */
+  adLogin: {
+    path: 'adLogin',
+    title: '管理員登入'
+  },
+
+  /**
+   * 首頁
+   */
+  home: {
+    path: 'home',
+    title: '首頁'
+  },
+
+  /**
+   * 我的最愛
+   */
+  favorite: {
+    path: 'favorite',
+    title: '我的最愛'
+  },
+
+  /**
+   * 詳細資訊
+   */
+  detail: {
+    path: 'detail',
+    title: '詳細資訊'
+  },
+
+  /**
+   * 建議與回饋
+   */
+  feedback: {
+    path: 'feedback',
+    title: '建議與回饋'
+  },
+
+  /**
+   * 意見處理
+   */
+  issues: {
+    path: 'issues',
+    title: '意見處理'
+  },
+
+  /**
+   * 歸檔列表
+   */
+  archive: {
+    path: 'archive',
+    title: '歸檔列表'
+  },
+
+  /**
+   * 意見詳情
+   */
+  issueDetail: {
+    path: 'issue-detail',
+    title: '意見詳情'
+  },
+
+  /**
+   * 無管理權限
+   */
+  authError: {
+    path: 'auth-error',
+    title: '無管理權限'
+  }
+
+}
