9639a9203fdef2163eebfe60d304deaee87b4134
Joe Lin <>
2019-09-04 13:38:22 +0800

    [Circle CI] 調整lint錯誤

--- src/app/layout/header/header.module.ts
+++ src/app/layout/header/header.module.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './header.component';
 import { SharedModule } from '../../shared/shared.module';
 import { ConditionModalModule } from './condition-modal/condition-modal.module';
 
-
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
