59d60c176416a4e287fc88f5e9773925792ba958
Joe Lin <>
2019-09-17 12:57:50 +0800

    [Image-Modal] 補 Unit Test

--- src/app/shared/components/image-modal/image-modal.component.spec.ts
+++ src/app/shared/components/image-modal/image-modal.component.spec.ts
@@ -4,16 +4,20 @@ import { ImageModalComponent } from './image-modal.component';
 import { ModalRef } from '../modal/modal-ref.model';
 import { MODAL_DATA } from '../modal/modal';
 
+
 describe('ImageModalComponent', () => {
+
   let component: ImageModalComponent;
   let fixture: ComponentFixture<ImageModalComponent>;
+
+  const mockData = { url: 'a' };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ImageModalComponent],
       providers: [
         { provide: ModalRef, useValue: {} },
-        { provide: MODAL_DATA, useValue: { src: '' } },
+        { provide: MODAL_DATA, useValue: mockData },
       ]
     })
       .compileComponents();
@@ -28,5 +32,10 @@ describe('ImageModalComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('pictureUrl 應該帶入 MODAL_DATA 的資料', () => {
+    expect(component.pictureUrl).toBe(mockData.url);
+  });
+
 
 });
