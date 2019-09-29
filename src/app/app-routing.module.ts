80ad94d4c945cca100518e5afecfa6462fa60bfc
Joe Lin <>
2019-09-30 02:20:51 +0800

    [Router] use history mode

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,6 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes,
     {
-      useHash: true,
       preloadingStrategy: PreloadAllModules,
       onSameUrlNavigation: 'reload'
     })],
